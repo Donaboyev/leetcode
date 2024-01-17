832ad697eb162063c6ec5a3b7546437f23966340
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-01-17 18:19:30 +0500

    question 2810 is solved

---
+++ src/q2810/Main.java
@@ -0,0 +1,48 @@
+package q2810;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final Solution sl = new Solution();
+        System.out.println(sl.finalString("string"));
+    }
+}
+
+class Solution {
+    public String finalString(String s) {
+        final char[] letters = s.toCharArray();
+        char[] result = new char[letters.length];
+        int index = 0;
+        for (final char i : letters) {
+            if (i == 'i') {
+                int start = 0;
+                int end = index - 1;
+                while (start < end) {
+                    final char temp = result[start];
+                    result[start] = result[end];
+                    result[end] = temp;
+                    start++;
+                    end--;
+                }
+            } else {
+                result[index] = i;
+                index++;
+            }
+        }
+        return new String(result, 0, index);
+    }
+//    public String finalString(String s) {
+//        final char[] l = s.toCharArray();
+//        final StringBuilder b = new StringBuilder();
+//        for (final char i : l) {
+//            if (i == 'i') {
+//                b.reverse();
+//            } else {
+//                b.append(i);
+//            }
+//        }
+//        return b.toString();
+//    }
+}
