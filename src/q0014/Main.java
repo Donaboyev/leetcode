1e6bcb32dc69fe00dc50d8c3bc2adeb600ea894b
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-01-21 23:18:53 +0500

    question 14 is solved

--- src/q0014/Main.java
+++ src/q0014/Main.java
@@ -1,7 +1,5 @@
 package q0014;
 
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
         final Solution sn = new Solution();
@@ -11,15 +9,39 @@ public class Main {
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        final StringBuilder s = new StringBuilder();
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i].isEmpty()) {
-                return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
             }
-//            for (int j = i; j < ; j++) {
-//
-//            }
         }
-        return "";
+        return prefix;
     }
+//    public String longestCommonPrefix(String[] strs) {
+//        if (strs.length == 1) {
+//            return strs[0];
+//        }
+//        int count = 200;
+//        final char[] letters = strs[0].toCharArray();
+//        for (int j = 1; j < strs.length; j++) {
+//            int innerCount = 0;
+//            final char[] secondLetters = strs[j].toCharArray();
+//            final int min = Math.min(letters.length, secondLetters.length);
+//            for (int k = 0; k < min; k++) {
+//                if (letters[k] == secondLetters[k]) {
+//                    innerCount++;
+//                    continue;
+//                }
+//                break;
+//            }
+//            if (innerCount == 0) {
+//                return "";
+//            }
+//            if (count > innerCount) {
+//                count = innerCount;
+//            }
+//
+//        }
+//        return strs[0].substring(0, count);
+//    }
 }
