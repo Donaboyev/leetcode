34e2b7e0d5d7573d878997dfde8e2d7f159ef968
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-01-23 08:24:32 +0500

    question 28 is solved

---
+++ src/q0028/Main.java
@@ -0,0 +1,41 @@
+package q0028;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        final Solution sn = new Solution();
+        System.out.println(sn.strStr("leetcode", "leeto"));
+    }
+}
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        final char[] hayLetters = haystack.toCharArray();
+        final char[] needleLetters = needle.toCharArray();
+        int index = -1;
+        for (int i = 0; i < hayLetters.length; i++) {
+            if (hayLetters.length - i < needleLetters.length) {
+                return -1;
+            }
+            boolean contains = true;
+            for (int j = 0; j < needleLetters.length; j++) {
+                if (hayLetters[i + j] == needleLetters[j]) {
+                    if (j == 0) {
+                        index = i;
+                    }
+                } else {
+                    contains = false;
+                    break;
+                }
+            }
+            if (contains) {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
