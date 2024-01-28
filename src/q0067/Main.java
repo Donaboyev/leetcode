70fda996b0645c2e7cd5de56e861016df0382818
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-01-28 13:32:59 +0500

    question 67 is solved

--- src/q0067/Main.java
+++ src/q0067/Main.java
@@ -3,30 +3,52 @@ package q0067;
 public class Main {
     public static void main(String[] args) {
         final Solution sn = new Solution();
-        System.out.println(sn.addBinary("11", "1"));
+        System.out.println(sn.addBinary("0", "0"));
     }
 }
 
 class Solution {
     public String addBinary(String a, String b) {
         final int maxLength = Math.max(a.length(), b.length());
-        final char[] aDigits = a.toCharArray();
-        final char[] bDigits = b.toCharArray();
-        char[] result = new char[maxLength + 1];
-        char carryOver = '0';
+        final char[] aDigits = new char[maxLength];
+        final char[] bDigits = new char[maxLength];
         for (int i = maxLength - 1; i >= 0; i--) {
-            char aCurrent = '0';
-            char bCurrent = '0';
-            if (i < aDigits.length) {
-                aCurrent = aDigits[i];
+            aDigits[i] = (a.length() - (maxLength - i) >= 0) ? a.charAt(a.length() - (maxLength - i)) : '0';
+        }
+        for (int i = maxLength - 1; i >= 0; i--) {
+            bDigits[i] = (b.length() - (maxLength - i) >= 0) ? b.charAt(b.length() - (maxLength - i)) : '0';
+        }
+        final char[] result = new char[maxLength + 1];
+        char carryOver = '0';
+        for (int i = maxLength; i >= 0; i--) {
+            if (i == 0) {
+                result[i] = carryOver == '0' ? ' ' : '1';
+                break;
             }
-            if (i < bDigits.length) {
-                bCurrent = bDigits[i];
-            }
+            char aCurrent = aDigits[i - 1];
+            char bCurrent = bDigits[i - 1];
             if (aCurrent == '1' && bCurrent == '1') {
-                result[i] = '1';
+                if (carryOver == '1') {
+                    result[i] = '1';
+                } else {
+                    result[i] = '0';
+                    carryOver = '1';
+                }
+            } else if ((aCurrent == '0' && bCurrent == '1') || (aCurrent == '1' && bCurrent == '0')) {
+                if (carryOver == '1') {
+                    result[i] = '0';
+                } else {
+                    result[i] = '1';
+                }
+            } else {
+                if (carryOver == '1') {
+                    result[i] = '1';
+                    carryOver = '0';
+                } else {
+                    result[i] = '0';
+                }
             }
         }
-        return "";
+        return new String(result).trim();
     }
 }
