7996ceeeff54dd336a0452d38d73b2e200ac6c7d
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-11-27 18:35:55 +0500

    ✨ Refactor: Simplify `isUgly` function
    This commit simplifies the `isUgly` function by removing unnecessary checks and iterations.  It now directly checks for divisibility by 2, 3, and 5, and returns whether the remaining number is 1.

--- src/easy/q0263/Main.kt
+++ src/easy/q0263/Main.kt
@@ -1,44 +1,20 @@
 package easy.q0263
 
-import kotlin.math.max
-import kotlin.math.sqrt
-
 fun main() {
     val sl = Solution()
-    println(sl.isUgly(11))
+    println(sl.isUgly(6))
+    println(sl.isUgly(1))
+    println(sl.isUgly(14))
+    println(sl.isUgly(22))
 }
 
 private class Solution {
     fun isUgly(n: Int): Boolean {
-        if (n < 1) {
-            return false
-        }
-        if (n == 1) {
-            return true
-        }
-        var current = n
-        var isPrime = true
-        while (current % 2 == 0) {
-            current /= 2
-            isPrime = false
-        }
-        while (current % 3 == 0) {
-            current /= 3
-            isPrime = false
-        }
-        while (current % 5 == 0) {
-            current /= 5
-            isPrime = false
-        }
-        if (current > 6) {
-            val max = max(49, current)
-            for (i in 7..sqrt(max.toDouble()).toInt()) {
-                if (current % i == 0) {
-                    return false
-                }
-                current /= i
-            }
-        }
-        return !isPrime
+        if (n <= 0) return false
+        var number = n
+        while (number % 2 == 0) number /= 2
+        while (number % 3 == 0) number /= 3
+        while (number % 5 == 0) number /= 5
+        return number == 1
     }
 }
